56d7d116143a9851d62a637aa1d0a7350110ae8b
kapil11Agnihotri <kapil90444@gmail.com>
2022-11-04 17:25:29 +0530

    callback implemented

--- src/App.js
+++ src/App.js
@@ -1,29 +1,47 @@
-import React, { useState } from "react";
+import React, { useState, useEffect, useCallback } from "react";
 
 import MoviesList from "./components/MoviesList";
 import "./App.css";
 
 function App() {
   const [movies, setMovies] = useState([]);
-  const [isLoading,setIsLoading]=useState(false)
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
 
-  async function fetchMoviseHandler() {
-    setIsLoading(true)
-    const response = await fetch("https://swapi.dev/api/films/");
-    const data = await response.json();
 
-    const transformedMovies = data.results.map((movieData) => {
-      return {
-        id: movieData.episode_id,
-        title: movieData.title,
-        releaseDate: movieData.release_date,
-        openingText: movieData.opening_crawl,
-      };
-    });
+  const fetchMoviseHandler = useCallback(async () => {
+    setIsLoading(true);
+    setError(null);
+    try {
+      const response = await fetch("https://swapi.dev/api/films/");
 
-    setMovies(transformedMovies);
-    setIsLoading(false)
-  }
+      if (!response.ok) {
+        throw new Error("Something went wrong ....Retrying");
+      }
+
+      const data = await response.json();
+
+      const transformedMovies = data.results.map((movieData) => {
+        return {
+          id: movieData.episode_id,
+          title: movieData.title,
+          releaseDate: movieData.release_date,
+          openingText: movieData.opening_crawl,
+        };
+      });
+
+      setMovies(transformedMovies);
+      setIsLoading(false);
+    } catch (error) {
+      setError(error.message);
+    }
+    setIsLoading(false);
+  })
+
+  useEffect(() => {
+    fetchMoviseHandler();
+  }, []);
+
 
   return (
     <React.Fragment>
@@ -33,6 +51,7 @@ function App() {
       <section>
         {!isLoading && <MoviesList movies={movies} />}
         {isLoading && <h3>Loading MovieList.......</h3>}
+        {!isLoading && error && <p>{error}</p>}
       </section>
     </React.Fragment>
   );
